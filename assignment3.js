432a53a209b24a8d3b83449d98528f864350ae59
SHIKHA-DAS <89566689+SHIKHA-DAS@users.noreply.github.com>
2021-08-28 17:19:29 +0530

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,21 @@
+
+
+function all image()
+{
+document.getElementById(#change)
+
+
+function doll1 image()
+{
+document.getElementById(#doll1)
+}
+
+
+function doll2 image()
+{
+document.getElementById(#doll2)
+}
+function doll3 image()
+{
+document.getElementById(#doll3)
+}}
